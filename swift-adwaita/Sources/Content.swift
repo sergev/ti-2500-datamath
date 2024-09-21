fcf1fb978fc75428b7d55352207e234406eca141
Serge <serge.vakulenko@gmail.com>
2024-09-21 00:18:33 -0700

    Enhance contents.

--- swift-adwaita/Sources/Content.swift
+++ swift-adwaita/Sources/Content.swift
@@ -18,18 +18,19 @@ struct Content: View {
 
     var view: Body {
         VStack {
-            Text("TODO display")
-                .padding(10)
+            Text("12345678.9")
+                .title1()
+                .padding(20)
             HStack {
                 Button("CE") {
                     //proxy.send("e")
-                }
+                }.pill()
                 Button("÷") {
                     //proxy.send("/")
-                }
+                }.pill()
                 Button("×") {
                     //proxy.send("*")
-                }
+                }.pill()
                 VStack {
                     Text("chain")
                     //TODO: Image(systemName: "lightswitch.off.fill")
@@ -39,55 +40,55 @@ struct Content: View {
             HStack {
                 Button("7") {
                     //proxy.send("7")
-                }
+                }.pill()
                 Button("8") {
                     //proxy.send("8")
-                }
+                }.pill()
                 Button("9") {
                     //proxy.send("9")
-                }
+                }.pill()
                 Button("C") {
                     //proxy.send("c")
-                }
+                }.pill()
             }
             HStack {
                 Button("4") {
                     //proxy.send("4")
-                }
+                }.pill()
                 Button("5") {
                     //proxy.send("5")
-                }
+                }.pill()
                 Button("6") {
                     //proxy.send("6")
-                }
+                }.pill()
                 Button("-") {
                     //proxy.send("-")
-                }
+                }.pill()
             }
             HStack {
                 Button("1") {
                     //proxy.send("1")
-                }
+                }.pill()
                 Button("2") {
                     //proxy.send("2")
-                }
+                }.pill()
                 Button("3") {
                     //proxy.send("3")
-                }
+                }.pill()
                 Button("+") {
                     //proxy.send("+")
-                }
+                }.pill()
             }
             HStack {
                 Button("0") {
                     //proxy.send("0")
-                }
+                }.pill()
                 Button("·") {
                     //proxy.send(".")
-                }
+                }.pill()
                 Button("=") {
                     //proxy.send("=")
-                }
+                }.pill()
             }
         }
         .padding(20)
